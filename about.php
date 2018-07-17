3f83c81141f4fe6343aa2bf71d2f9160b00812ca
Artem Kamkov <tayler20166@gmail.com>
2018-07-18 00:18:50 +0300

    Initial commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About - Git Test</title>
+</head>
+<body>
+<div class="page_content">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aperiam atque beatae cum doloribus exercitationem facere, ipsa ipsam iusto maxime modi molestias ratione tempore vitae. Illo iste quos ullam.</p>
+</div>
+
+</body>
+</html>
